fb1010d67c012ee6a59c2d51e65e189ca1bbfe69
frank-booth <jfraley519@gmail.com>
2022-09-19 12:23:45 -0400

    home page started

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Container, Image } from 'semantic-ui-react'
+import Picture from '../assets/jeff.jpeg'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className="home-container">
+      <Container textAlign="center">
+        <Image
+          className="profile-pic"
+          src={Picture}
+          size="small"
+          centered
+          circular
+        />
+        <h1>Jeff Fraley</h1>
+        <h3>Software Engineer</h3>
+      </Container>
+    </div>
+  )
 }
 
 export default Home
