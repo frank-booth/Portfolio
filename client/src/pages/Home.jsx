21a7fa6b2721999f8697cf5ae094fb0fcadfbd96
frank-booth <jfraley519@gmail.com>
2022-09-19 16:36:59 -0400

    majority of the home page is complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,7 +18,6 @@ const Home = () => {
               circular
             />
           </NavLink>
-
           <h1 className="headings">Jeff Fraley</h1>
           <h3 className="headings">Software Engineer</h3>
         </Container>
@@ -27,17 +26,30 @@ const Home = () => {
         <div className="about-me">
           <h2>About Me</h2>
           <p>
-            I am a multi-faceted software engineer with a unique combination of
-            philanthropic, business and banking experience. A life-long learner
-            who loves creating through software development. As a resourceful
-            problem solver and consensus builder, I possess an innate ability to
-            build and invest in high-performance teams and donor/client
-            relationships.
+            Jeff Fraley is a multi-faceted software engineer with a unique
+            combination of philanthropic, business and banking experience. A
+            life-long learner who loves creating through software development.
+            As a resourceful problem solver and consensus builder, he possess an
+            innate ability to build and invest in high-performance teams and
+            donor/client relationships.
           </p>
         </div>
         <div className="contact">
           <h2>Contact</h2>
-          {/* <Icon name="linkedin" as="href='www.twitter.com'" /> */}
+          <a href="https://www.linkedin.com/in/jeff-fraley/" target="_blank">
+            <Icon color="black" name="linkedin" size="big" />
+            <span className="icons">LinkedIn</span>
+          </a>
+          <br />
+          <a href="https://github.com/frank-booth" target="_blank">
+            <Icon color="black" name="github" size="big" />
+            <span className="icons">GitHub</span>
+          </a>
+          <br />
+          <a href="mailto:jfraley519@gmail.com" target="_blank">
+            <Icon color="black" name="mail" size="big" />
+            <span className="icons">Email</span>
+          </a>
         </div>
       </div>
     </div>
