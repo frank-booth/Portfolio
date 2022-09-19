ecf50b9e331318c9cd8c702fae0ce760913daeea
frank-booth <jfraley519@gmail.com>
2022-09-19 11:17:29 -0400

    installed sematic Ui

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello</header>
+      <Button primary>Test</Button>
+    </div>
+  )
+}
+
+export default App
