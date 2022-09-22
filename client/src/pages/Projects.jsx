fd4b4ed8aaea03b5e79a175d7b223d2157257815
frank-booth <jfraley519@gmail.com>
2022-09-22 14:33:04 -0400

    add project 2

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import { Container, Image, Segment } from 'semantic-ui-react'
 import Whack from '../assets/whack_a_mole.png'
+import Bond from '../assets/bond.png'
 
 const Projects = () => {
   return (
@@ -17,9 +18,35 @@ const Projects = () => {
           </Container>
         </div>
       </div>
-      <div className="whack-a-mole-container">
+      <div className="project3-container">
         <p className="whack-a-mole-text">
-          <Image src={Whack} size="medium" floated="left" />
+          <Image src={Bond} size="medium" floated="left" />
+          <span className="project-title">Bond, James Bond</span>
+          <br />
+          <br />
+          I designed and developed a James Bond site to show all things Bond.
+          Built with ExpressJS, NodeJS, MongoDB and Mongoose Database.
+          <br />
+          <a
+            href="https://github.com/frank-booth/Bond-James-Bond"
+            target="_blank"
+          >
+            <span>GitHub</span>
+          </a>
+          <a href="https://fraley-project1.surge.sh" target="_blank">
+            <span> / Demo</span>
+          </a>
+        </p>
+        <br />
+      </div>
+      <div className="project4-container">
+        <p className="whack-a-mole-text">
+          <Image
+            src={Whack}
+            size="medium"
+            floated="left"
+            className="whack-image"
+          />
           <span className="project-title">Whack-A-Mole</span>
           <br />
           <br />
